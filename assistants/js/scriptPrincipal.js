dcd5fc2b8189b3f651051777d8dcbb9d1593edb2
Joao <joao.menes@hotmail.com>
2022-09-28 21:36:09 -0300

    Confirmação de texto

--- assistants/js/scriptPrincipal.js
+++ assistants/js/scriptPrincipal.js
@@ -1,18 +1,18 @@
 const botaoFanfic = document.querySelector('.btn-fanfic');
 const listaDeFanfics = document.querySelector('#listaDeFanfics');
-const textoFanfic = document.querySelector('.textFanfic');
-const titutloFanfic = document.querySelector('.tituloStory');
+const textoFanfic = document.querySelector('.textFanfic');                             //condição para publicação de texto e titulo aplicada.
+const titutloFanfic = document.querySelector('.titulo-fanfic');
 
 botaoFanfic.addEventListener('click', function (e) {
 
-    if (!textoFanfic.value) return; //quando tiver nada o retorno será nada. se não "!" tiver o valor de  textFanfic
+    if (!textoFanfic.value | !titutloFanfic.value) return confirm("é necessário ter um texto para ser publicado!"); //quando tiver nada o retorno será nada. se não "!" tiver o valor de  textFanfic
     //caso contrario jogar o texto na lista
     const confirma = confirm("Confirma a publicação dessa Fanfic?")
     if (confirma == true) {
-        criarStory((textoFanfic.value));
+        criarStory((textoFanfic.value | titutloFanfic.value));
         limpaAreaTexto();
-    } else (alert("A Fanfic não vai ser publicada"));
-});
+    } if (confirma == false) {document.location.reload(true);}  //irá recarregar a página caso não for confirmado a publicação
+    });
 
 function criarLi() {
     const li = document.createElement('li');  //retorna li 
@@ -57,5 +57,5 @@ function adicionandoHistorias() {  //converte para formato string novamente
 adicionandoHistorias();
 
 
-//pegar apenas o título do texto
+//pegar apenas o título do texto e sem titulo n tem publicação
 //link cada titulo ao clique abri-lo para leitura do texto
